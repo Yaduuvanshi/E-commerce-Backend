e17a999b40ca1623ec171b8bacab46122cf3f519
Yaduuvanshi <amitkumar09022005@gmail.com>
2025-08-04 21:34:52 +0530

    rect:

--- server.js
+++ server.js
@@ -28,7 +28,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "http://localhost:5173",
+    origin: ["http://localhost:5173","https://e-commerce-frontend-ivory-nu.vercel.app"],
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
